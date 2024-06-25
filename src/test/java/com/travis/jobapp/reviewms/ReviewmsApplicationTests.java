94e90849fe68a7858576ada7ab3741a35b2cac1e
Luis Lomba Martínez <luis.lomba@gruposantander.com>
2024-06-25 22:55:42 +0200

    removed unnecesary test

--- src/test/java/com/travis/jobapp/reviewms/ReviewmsApplicationTests.java
+++ src/test/java/com/travis/jobapp/reviewms/ReviewmsApplicationTests.java
@@ -1,13 +1,13 @@
 package com.travis.jobapp.reviewms;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ReviewmsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
